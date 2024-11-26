b269951b2351f91290a898aa0c64d30a22ffbe4d
David L. Enock <enockdavid55@gmail.com>
2024-11-26 09:39:44 +0300

    updates on json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import data from "../data.json";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import CountryDetails from "./components/CountryDetails";
 
 const App = () => {
-  const [countries, setCountries] = useState(data);
-
   return (
     <>
       <Navbar />
       <Home />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="country/:countryName"
-            element={<CountryDetails countries={countries} />}
-          />
-        </Routes>
-      </BrowserRouter>
     </>
   );
 };
